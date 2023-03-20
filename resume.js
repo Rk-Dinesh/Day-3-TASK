113df001968a6427a76b04a835c764f443319e77
Rk-Dinesh <127471925+Rk-Dinesh@users.noreply.github.com>
2023-03-20 11:09:21 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,50 @@
+let myResume = {
+    'bio' : {
+        "name" : "xyz",
+        "email": "abc@gmail.com",
+        "phone": "0123456789",
+    },
+
+     'address': {
+        "house no" : "x/y",
+        "street"  : "abc street",
+        "city" : "salem",
+        "state" : "tamilNadu",
+        "pincode" : 600001,
+     },
+     "10 th education": [
+        {
+          "institution": " College Of Engineering",
+          "department": "Automobile",
+          "studyType": "fulltime",
+          "batch start year": 2017,
+          "batch end year": 2021,
+          "gpa": 7.5,
+        }
+      ],
+      "12 th education": [
+        {
+            "institution": "zyx HRSC",
+            "department": "Maths - Bio",
+            "batch year": 2015,
+            "percentage": 92.5,
+        }
+      ],
+      "10 th education": [
+        {
+            "institution": "xyz Matric School",
+            "batch year": 2013,
+            "percentage": 98.5,
+        }
+      ],
+      "skills": [
+        {
+            "lanuage": "Tamil,English",
+            "software skills": "Beginner in CODING",
+            "hobbby": ["reading books","playing outdoor","cycling"],
+            "softskills":["Adaptable to stress enivironment","Good  at leadership","quicklearner"],
+        }
+      ]
+            
+}
+console.log(myResume);
